e58b9096dc3ddbc630aa0cb82bcc6d15d349b235
unknown <faridwilhelmzimmermann1111@gmail.com>
2025-01-31 09:31:18 +0100

    fixed bugs in index.ts regarding using apollo server as express middleware, updated imports

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,20 @@
-import { ApolloServer } from "apollo-server";
+import { ApolloServer } from "@apollo/server"; // Apollo Server 4
+import { expressMiddleware } from "@apollo/server/express4"; // Express middleware for Apollo 4
+import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
 import typeDefs from "./schema";
 import resolvers from "./resolvers";
 import { verifyToken } from "../helpers/auth";
 
 dotenv.config();
+
+const app = express();
+
+app.use(cors()); 
+app.use(bodyParser.json()); 
 
 const MONGO_URI = process.env.MONGO_URI as string;
 const SECRET_KEY = process.env.SECRET_KEY as string;
@@ -16,14 +25,31 @@ mongoose.connect(MONGO_URI)
   .catch(err => console.log(err));
 
 
-  const server = new ApolloServer({ typeDefs, resolvers , 
-    context: ({ req }) => {
-      const token = req.headers.authorization || "";
-      const user = verifyToken(token, SECRET_KEY);
-      return {user};
-  }});
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
-  server.listen().then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`);
+async function startServer() {
+  await server.start(); 
 
+  app.use(
+    "/graphql",
+    expressMiddleware(server, {
+      context: async ({ req }) => {
+        const token = req.headers.authorization || "";
+        const user = verifyToken(token, SECRET_KEY as string);
+        return { user };
+      },
+    })
+  );
+
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/graphql`);
   });
+}
+  
+  
+
+startServer();
