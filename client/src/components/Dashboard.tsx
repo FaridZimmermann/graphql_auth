38ae2a0b9b31ff11da94243c9f46f5d5a47a92ee
unknown <faridwilhelmzimmermann1111@gmail.com>
2025-02-01 10:06:19 +0100

    added Dashboard.tsx

---
+++ client/src/components/Dashboard.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../redux/appSlice";
+import { useNavigate } from "react-router-dom";
+
+const Dashboard: React.FC = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  
+  const user = useSelector((state: any) => state.app.user);
+
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate("/login");
+  };
+
+  return (
+    <div className="">
+      <div className="">
+        <h2 className="">Welcome, {user?.email}!</h2>
+        <p className="">You are logged in.</p>
+        <button
+          onClick={handleLogout}
+          className=""
+        >
+          Logout
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
